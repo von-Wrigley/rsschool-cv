08b7d3d3eecc61c470d7ff78fc6eaa9af6ff3f0c
von-Wrigley <119867977+von-Wrigley@users.noreply.github.com>
2023-04-12 23:04:35 +0300

    feat: double

--- shelter/burger-menu.js
+++ shelter/burger-menu.js
@@ -1,6 +1,25 @@
- const hamburger = document.querySelector(".burger-menu");  
- const navsub = document.querySelector(".bg-nav");  
- hamburger.addEventListener('click', () => {  
-  hamburger.classList.toggle("change")  
-  navsub.classList.toggle("nav-change")  
- });  
+
+var burgerMenu = document.getElementById('burger');
+var overlay = document.getElementById('burger-menu');
+
+burgerMenu.addEventListener('click',function(){
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+
+});
+
+
+var secondslide = document.getElementById('wrapper-firstslide2');
+var header = document.getElementById('header');
+var body = document.getElementsByTagName('body')[0]
+burgerMenu.onclick = function() {
+    body.classList.toggle("dark");
+    secondslide.classList.toggle("background");
+    header.classList.toggle("background");
+};
+
+secondslide.onclick = function() {
+    overlay.classList.toggle("hide");
+    this.classList.toggle("light");
+    burgerMenu.classList.toggle("open");
+}; 
